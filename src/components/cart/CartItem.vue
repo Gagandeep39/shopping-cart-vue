0b532b1fffc85afe3867fbe481ae41d34925f0d1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 12:52:15 +0000

    Fixed remove from cart button

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -31,7 +31,7 @@ export default {
   },
   methods: {
     remove() {
-      this.$store.dispatch('removeFromCart', { prodId: this.prodId });
+      this.$store.dispatch('cart/removeFromCart', { prodId: this.prodId });
     },
   },
 };
